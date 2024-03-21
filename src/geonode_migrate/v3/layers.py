21c3463a130e18accf20f9a8bed2d3219dc02484
Niccolò Cantù <niccolo.cantu@nina.no>
2024-03-21 12:50:23 +0100

    v3: handle errors in layer download

--- src/geonode_migrate/v3/layers.py
+++ src/geonode_migrate/v3/layers.py
@@ -28,37 +28,40 @@ def pull_layers(conf: Config):
         if not conf.force and table.contains(doc_id=doc['id']):
             click.echo(f'already downloaded {doc["id"]} - {doc["title"]}')
             continue
+        
+        try:
+            response = conf.session.get(f'{conf.base_url}/api/layers/{doc["id"]}')
+            data = response.json()
 
-        response = conf.session.get(f'{conf.base_url}/api/layers/{doc["id"]}')
-        data = response.json()
+            layer_dir = dir / str(doc["id"])
+            layer_dir.mkdir(exist_ok=True, parents=True)
 
-        layer_dir = dir / str(doc["id"])
-        layer_dir.mkdir(exist_ok=True, parents=True)
+            data['__files__'] = {}
 
-        data['__files__'] = {}
+            if data["storeType"] == "coverageStore":
+                extension = 'tiff'
+                response = conf.geoserver_session.get(f"{conf.base_url}/geoserver/wcs?format=image%2Ftiff&request=GetCoverage&version=2.0.1&service=WCS&coverageid={data['typename']}")
+                data["__files__"]["base_file"] = str(layer_dir / f'{data["name"]}.{extension}')
 
-        if data["storeType"] == "coverageStore":
-            extension = 'tiff'
-            response = conf.geoserver_session.get(f"{conf.base_url}/geoserver/wcs?format=image%2Ftiff&request=GetCoverage&version=2.0.1&service=WCS&coverageid={data['typename']}")
-            data["__files__"]["base_file"] = str(layer_dir / f'{data["name"]}.{extension}')
+                with open(data["__files__"]["base_file"], 'wb') as f:
+                    f.write(response.content)
+            else:
+                response = conf.geoserver_session.get(f"{conf.base_url}/geoserver/geonode/ows?service=WFS&version=1.0.0&request=GetFeature&typeName={data['typename']}&outputFormat=SHAPE-ZIP")
 
-            with open(data["__files__"]["base_file"], 'wb') as f:
+                with zipfile.ZipFile(io.BytesIO(response.content)) as zip:
+                    files = [n for n in zip.namelist() if any([n.endswith(ext) for ext in SHP_ALLOWED_EXT.keys()])]
+                    for f in files:
+                        ext = Path(f).suffix
+                        zip.extract(f, path=str(layer_dir))
+                        data["__files__"][SHP_ALLOWED_EXT[ext]] = str(layer_dir / f)
+            
+            data["__files__"]['sld_file'] = str(layer_dir / f'{data["name"]}.sld')
+            response = conf.geoserver_session.get(f'{conf.base_url}/geoserver/rest/styles/{data["name"]}.sld')
+            with open(data["__files__"]['sld_file'], 'wb') as f:
                 f.write(response.content)
-        else:
-            response = conf.geoserver_session.get(f"{conf.base_url}/geoserver/geonode/ows?service=WFS&version=1.0.0&request=GetFeature&typeName={data['typename']}&outputFormat=SHAPE-ZIP")
+            
+            table.upsert(Document(data, doc_id=data['id']))
 
-            with zipfile.ZipFile(io.BytesIO(response.content)) as zip:
-                files = [n for n in zip.namelist() if any([n.endswith(ext) for ext in SHP_ALLOWED_EXT.keys()])]
-                for f in files:
-                    ext = Path(f).suffix
-                    zip.extract(f, path=str(layer_dir))
-                    data["__files__"][SHP_ALLOWED_EXT[ext]] = str(layer_dir / f)
-        
-        data["__files__"]['sld_file'] = str(layer_dir / f'{data["name"]}.sld')
-        response = conf.geoserver_session.get(f'{conf.base_url}/geoserver/rest/styles/{data["name"]}.sld')
-        with open(data["__files__"]['sld_file'], 'wb') as f:
-            f.write(response.content)
-        
-        table.upsert(Document(data, doc_id=data['id']))
-
-        click.echo(f'downloaded {doc["id"]} - {doc["title"]}')
+            click.echo(f'downloaded {doc["id"]} - {doc["title"]}')
+        except Exception as e:
+            click.echo(f'error {doc["id"]} - {doc["title"]}: {e}', err=True)
