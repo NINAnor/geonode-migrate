1866729eae00faa51e95ae038bda2ff4f8ddc27d
Niccolò Cantù <niccolo.cantu@nina.no>
2024-04-08 10:04:00 +0200

    v3: pull groups with logo

--- src/geonode_migrate/v3/users.py
+++ src/geonode_migrate/v3/users.py
@@ -2,6 +2,7 @@ from ..config import Config
 from ..utils import generate_password
 from tinydb.database import Document
 import click
+import pathlib
 
 
 def pull_users(conf: Config):
@@ -22,10 +23,13 @@ def pull_users(conf: Config):
         table.upsert(Document(doc, doc_id=doc['id']))
         click.echo(f'downloaded {doc["id"]} - {doc["username"]}')
     
+
+def pull_groups(conf: Config):
+    table = conf.db.table('groups')
     response = conf.session.get(f'{conf.base_url}/api/groups/')
     data = response.json()
-
-    table = conf.db.table('groups')
+    docs_dir = conf.output_path / 'groups'
+    docs_dir.mkdir(parents=True, exist_ok=True)
 
     click.echo(f"Found groups: {data['meta']['total_count']}")
 
@@ -33,6 +37,17 @@ def pull_users(conf: Config):
         if not conf.force and table.contains(doc_id=doc['id']):
             click.echo(f'already downloaded {doc["id"]} - {doc["title"]}')
             continue
+
+        if doc['logo']:
+            logo = pathlib.Path(doc['logo'])
+            doc["file_position"] = str(docs_dir / logo.name )
+            print(doc["file_position"])
+
+            response = conf.session.get(f"{conf.base_url}{doc['logo']}")
+            with open(doc["file_position"], 'wb') as f:
+                f.write(response.content)
+        else:
+            doc["file_position"] = None
 
         table.upsert(Document(doc, doc_id=doc['id']))
         click.echo(f'downloaded {doc["id"]} - {doc["title"]}')
